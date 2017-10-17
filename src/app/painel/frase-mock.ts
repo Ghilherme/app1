35d578363cd6dca01be157e94259090f1f2ac116
Guilherme Pereira de Oliveira - GOI <guilherme.oliveira@iteris.local>
2017-10-17 19:07:15 -0200

    Ok

---
+++ src/app/painel/frase-mock.ts
@@ -0,0 +1,9 @@
+import {Frase} from '../shared/frase.model'
+
+const FRASES: Frase[] = [
+    {fraseEng: "I want you", fraseBr:"Eu quero você"},
+    {fraseEng: "I like you", fraseBr:"Eu gosto de você"},
+    {fraseEng: "I watch TV", fraseBr:"Eu assisto TV"},
+    {fraseEng: "It has bread in the sky?", fraseBr:"Tem pão no céu?"}
+
+]
