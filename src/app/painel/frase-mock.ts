9f20f03a855171df9267bdb8ef88994824a10754
Guilherme <g.p.oliveira@hotmail.com>
2017-10-20 09:59:23 -0200

    ok

--- src/app/painel/frase-mock.ts
+++ src/app/painel/frase-mock.ts
@@ -1,6 +1,6 @@
 import {Frase} from '../shared/frase.model'
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
     {fraseEng: "I want you", fraseBr:"Eu quero você"},
     {fraseEng: "I like you", fraseBr:"Eu gosto de você"},
     {fraseEng: "I watch TV", fraseBr:"Eu assisto TV"},
